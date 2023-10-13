d58ae7bb1b0562d97b40626e927915adfe3f7c8c
JuanLaborde <juan.laborde33@gmail.com>
2023-10-13 03:31:48 +0200

    Build Application

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { textContainer, textVariant, textVariant2 } from "../utils/motion";
 import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
@@ -14,17 +14,14 @@ export const TypingText = ({ title, textStyles }) => (
       </motion.span>
     ))}
   </motion.p>
-
-
 );
-
 export const TitleText = ({ title, textStyles }) => (
   <motion.h2
-   variants = {textVariant2}
-   initial = 'hidden'
-   whileInView='show'
-   className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
-   {title}
+    {title}
   </motion.h2>
 );
